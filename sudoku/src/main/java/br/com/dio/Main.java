68456c065f890384eb6aa4b8600ac7f0d1f93cbe
Thaize Paulo <thaizepaulo@gmail.com>
2025-04-24 14:38:36 -0300

    Criação do MenuService

--- sudoku/src/main/java/br/com/dio/Main.java
+++ sudoku/src/main/java/br/com/dio/Main.java
@@ -1,6 +1,18 @@
 package br.com.dio;
 
+import java.util.stream.Stream;
+
+import static br.com.dio.service.MenuService.createMenu;
+import static java.util.stream.Collectors.toMap;
+
 public class Main {
+
     public static void main(String[] args) {
+
+        final var gameConfig = Stream.of(args)
+                .collect(toMap(k -> k.split(";")[0], v -> v.split(";")[1]));
+
+        createMenu();
+
     }
 }
---
+++ sudoku/src/main/java/br/com/dio/service/MenuService.java
@@ -0,0 +1,31 @@
+package br.com.dio.service;
+
+import java.util.Scanner;
+
+public class MenuService {
+
+    private final static Scanner scanner = new Scanner(System.in);
+
+    public static void createMenu() {
+        var option = -1;
+        while (true){
+            System.out.println("Selecione uma das opções a seguir");
+            System.out.println("1 - Iniciar um novo Jogo");
+            System.out.println("2 - Colocar um novo número");
+            System.out.println("3 - Remover um número");
+            System.out.println("4 - Visualizar jogo atual");
+            System.out.println("5 - Verificar status do jogo");
+            System.out.println("6 - limpar jogo");
+            System.out.println("7 - Finalizar jogo");
+            System.out.println("8 - Sair");
+
+            option = scanner.nextInt();
+
+            switch (option){
+                case 8 -> System.exit(0);
+                default -> System.out.println("Opção inválida, selecione uma das opções do menu");
+            }
+        }
+    }
+
+}
